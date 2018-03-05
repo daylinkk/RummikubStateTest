c7569727c5d1af927f6ccfaefc8d784b7e445132
snook20 <snook20@up.edu>
2018-03-05 14:00:21 -0800

    most of onclicklistener finished. still have to call action methods

--- app/src/main/java/com/example/daylinkuboyama/rummikubstatetest/MainActivity.java
+++ app/src/main/java/com/example/daylinkuboyama/rummikubstatetest/MainActivity.java
@@ -9,26 +9,56 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button runTestButton = (Button)findViewById(R.id.buttonRunTest);
-    EditText runTestText = (EditText) findViewById(R.id.editTextRunTest);
+    private Button runTestButton;
+    private EditText runTestText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RummikubState state= new RummikubState();
+        runTestButton = (Button)findViewById(R.id.buttonRunTest);
+        runTestText = (EditText)findViewById(R.id.editTextRunTest);
 
-        RummikubState state2 = new RummikubState(state, 0);
+        runTestButton.setOnClickListener(this);
 
-        Log.i("TAG",state.toString());
-        Log.i("TAG",state2.toString());
+        //RummikubState state= new RummikubState();
+
+        //RummikubState state2 = new RummikubState(state);
+
+        //Log.i("TAG",state.toString());
+        //Log.i("TAG",state2.toString());
     }
 
-    @Override
+    //@Override
     //listener for the runTest button
 
     public void onClick(View v) {
+        runTestText.setText("");
+
+        //creates a new instance of the game
+        RummikubState firstInstance = new RummikubState();
+
+        //calls copy constructor in rummikub state and makes copy of first instance
+        //perspective of player 1
+        RummikubState secondInstance = new RummikubState(firstInstance,0);
+
+        //below call action methods and print want is happening to edit text
+
+
+
+        //creates a new instance of gamestate
+        RummikubState thirdInstance = new RummikubState();
+
+        //creates a deep copy of third instance in perspective of player 1
+        RummikubState fourthInstance = new RummikubState(thirdInstance,0);
+
+        //call twoString methods on instance 2 and 4
+        //print both strings to edit text
+        runTestText.append(thirdInstance.toString());
+        runTestText.append(thirdInstance.toString());
+
+
 
     }
 
